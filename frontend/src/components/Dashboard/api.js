03f29bb973786de16eebe09d61eaf9d700aab0eb
Nandika koranga <90962321+nandikakoranga@users.noreply.github.com>
2024-07-01 20:44:15 +0530

    Update api.js

--- frontend/src/components/Dashboard/api.js
+++ frontend/src/components/Dashboard/api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'https://locohost:5000/api';
+const API_BASE_URL = 'https://localhost:5000/api';
 
 export const fetchData = async () => {
     try {
